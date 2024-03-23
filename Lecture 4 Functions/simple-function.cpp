776935291d04455ae316cb2b55a6fd1c12b3be61
thepixelcoder11 <mananshinde12@gmail.com>
2024-03-23 15:37:58 +0530

    initial commit

---
+++ Lecture 4 Functions/simple-function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void PrintLine (){
+    for(int i=0; i<10; i++)
+    {
+        cout << "sundar ko sundari pasand hai" << endl;
+    
+    }
+}
+
+int main()
+{
+    PrintLine();
+    return 0;
+}
